60671a6b4439362b82230fb17d1a2595548c6b35
MSixels <msixel22@gmail.com>
2024-05-14 18:42:11 -0300

    criando componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/MSixelS.png" />
+          <Avatar src="https://github.com/MSixelS.png" />
           <div className={styles.authorInfo}>
             <strong>Matheus Sixel</strong>
             <span>Dev Front-end</span>
